8f43a2e908f514c3da856b443cc7af0f46752579
Jinwoo Oh <ohjinwoo756@gmail.com>
2019-05-24 05:19:54 +0900

    [REFACTOR] code cleaned

--- src/application.py
+++ src/application.py
@@ -3,28 +3,32 @@ from task import Task
 class Application():
 
     def __init__(self, network, profile):
+        # Program input
         self.network = network
         self.profile = profile
         self.layer_list = list(network.layer)
         self.profile_list = list(profile.layer)
         self.network_len = len(self.layer_list)
 
-        self.tasks = [] # Combined version (network, profile)
-        self.name2task_dict = {}
-        self.make_tasks()
+        # Variables from input to real usage
+        self.tasks = []
+        self.name2task_dict = {} # hash table for speed up
+        self.make_tasks() # Input => Tasks
 
     def make_tasks(self):
-        self.make_initial_tasks()
+        self.make_initial_tasks() # Except for layer precedence
         self.apply_precedences()
-        # Example
-        # self.layer_list = [l for app in app_list for l in app.layer_list]
-        # print self.tasks[0].name
 
     def make_initial_tasks(self):
+        # Example of duplicate for loop
+        # self.layer_list = [l for app in app_list for l in app.layer_list]
+
+        # 1. From network input
         for index, net_l in enumerate(self.layer_list):
             self.tasks.append(Task(index, net_l)) # Make 'Task' object
             self.name2task_dict[self.tasks[index].name] = self.tasks[index] # register to dictionary
 
+        # 2. From profile input
         for index, prof_l in enumerate(self.profile_list):
             self.tasks[index].cpu = prof_l.cpu
             self.tasks[index].gpu = prof_l.gpu
@@ -40,13 +44,12 @@ class Application():
                 parent_obj.childs.append(self.tasks[l_idx]) # add chind info
 
         # debug
-        #for l_idx, net_l in enumerate(self.layer_list):
-        #    for idx in range(0, len(self.tasks[l_idx].childs)):
-        #        p1 = self.tasks[l_idx].childs[idx]
-        #        p2 = self.tasks[p1.index]
-        #        print p1 == p2
-        #        print "-----"
-        #    for idx in range(0, len(net_l.bottom)):
-        #        t1 = self.tasks[l_idx].parents[idx]
-        #        t2 = self.tasks[t1.index]
-        #        print t1 == t2
+        # for l_idx, net_l in enumerate(self.layer_list):
+        #     for idx in range(0, len(net_l.top)):
+        #         p1 = self.tasks[l_idx].childs[idx]
+        #         p2 = self.tasks[p1.index]
+        #         print "Child matched? ", p1 == p2
+        #     for idx in range(0, len(net_l.bottom)):
+        #         t1 = self.tasks[l_idx].parents[idx]
+        #         t2 = self.tasks[t1.index]
+        #         print "Parent matched? ", t1 == t2
--- src/main.py
+++ src/main.py
@@ -1,29 +1,38 @@
 import cmd_line_option_parser
 import input_reader
 
-# This application deals with only 1 dag yet.
-if __name__ == '__main__':
-
-    # Parse command line option
-    options = cmd_line_option_parser.parse_options()
-    # Read network inputs to make caffe_pb2.NetParameter variable
+def read_in_NetParameter(options):
     networks = []
     profiles = []
     for idx in range(0, len(options.networks)):
         networks.append(input_reader.read_network_prototxt(options.networks[idx]))
         profiles.append(input_reader.read_network_profile_prototxt(options.profiles[idx]))
-    # debug 
-    # print networks[0].layer[6].bottom
-    # print networks[0].layer[6].top
-    # print networks[1].layer[0].name
-    # print profiles[1].layer[0].cpu
+    return networks, profiles
 
+def make_scheduler_object(options, networks, profiles):
     # Load scheduler
     scheduler_module = __import__(options.scheduler) # Dynamic import != Static import
     scheduler_class = getattr(scheduler_module, options.scheduler) # (module name, class name)
     # The right below also possible
     # scheduler_class = getattr(scheduler_module, options.scheduler)(networks, profiles)
     scheduler_object = scheduler_class(networks, profiles) # Make class object
+
+    return scheduler_object
+
+if __name__ == '__main__':
+
+    # Parse command line option
+    options = cmd_line_option_parser.parse_options()
+
+    # Read inputs in caffe_pb2.NetParameter format
+    networks, profiles = read_in_NetParameter(options)
+    # debug 
+    # print networks[0].layer[6].bottom
+    # print networks[0].layer[6].top
+    # print networks[1].layer[0].name
+    # print profiles[1].layer[0].cpu
+
+    scheduler_object = make_scheduler_object(options, networks, profiles)
 
     # Scheduling
     scheduler_object.do_schedule()
